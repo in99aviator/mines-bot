96f2ee5d82603ec2d274e33c47199e61c806563e
modxcrazy <in99soft.dev.pvt.ltd@gmail.com>
2025-04-12 23:31:31 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -8,11 +8,15 @@ import {
 
 // Your Firebase Config
 const firebaseConfig = {
-  apiKey: "AIzaSyCn553qWsVz2VF1dZ4Ji5OkQDGFvMORbJE",
-  authDomain: "pg-data-ed1c2.firebaseapp.com",
-  databaseURL: "https://pg-data-ed1c2-default-rtdb.firebaseio.com",
-  projectId: "pg-data-ed1c2"
-};
+    apiKey: "AIzaSyCn553qWsVz2VF1dZ4Ji5OkQDGFvMORbJE",
+    authDomain: "pg-data-ed1c2.firebaseapp.com",
+    databaseURL: "https://pg-data-ed1c2-default-rtdb.firebaseio.com",
+    projectId: "pg-data-ed1c2",
+    storageBucket: "pg-data-ed1c2.firebasestorage.app",
+    messagingSenderId: "884858492190",
+    appId: "1:884858492190:web:67b7606ff790064ef3250f",
+    measurementId: "G-J73RD8EH7W"
+  };
 
 // Init Firebase
 const app = initializeApp(firebaseConfig);
